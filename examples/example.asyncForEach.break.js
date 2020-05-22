11a2942f3bf6234aa0f3cd40b8ab6a50a853eabc
Patrick P <mtcllf@gmail.com>
2020-05-22 08:56:48 +0200

    updated the asyncForEach Example

--- examples/example.asyncForEach.break.js
+++ examples/example.asyncForEach.break.js
@@ -1,15 +1,25 @@
 require('../index');
 
+// workaround for top level await
 (async () => {
     const arr = [1, 2, 3];
+    const another_array = ['Hello', 'World', 'meh'];
 
     await arr.asyncForEach(async (el, i, arr) => {
-        console.log('on iteration: ' + i);
+        console.log(`arr: on iteration: {i}`);
         if (i === 1) {
-            console.log('we should exit now');
+            console.log('arr: we should exit now');
             return 'break';
         }
         return new Promise(resolve => setTimeout(() => resolve(), 500));
     });
+
+    console.log('arr done');
+
+    await another_array.asyncForEach(async (el, i, arr) => {
+        console.log(`another_array: ${el}`);
+    });
+    console.log('another_array: done');
+    console.log('we are done now');
 })();
 
--- examples/example.forEach.break.js
+++ examples/example.forEach.break.js
@@ -3,7 +3,7 @@ require('../index');
 const arr = [1, 2, 3];
 
 arr.forEach((el, i, array) => {
-    console.log('on iteration: ' + i);
+    console.log(`on iteration: ${i}`);
     if (i === 1) {
         console.log('we should exit now');
         return 'break';
